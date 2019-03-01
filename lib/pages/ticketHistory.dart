2f656d51a4a7a8e44348c1481379dd0c1b215fb5
LorisC <loris@ciprin.fr>
2019-03-01 10:24:18 +0100

    Add decoration over the ticket name

--- lib/pages/ticketHistory.dart
+++ lib/pages/ticketHistory.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticketing/colors.dart';
 import 'package:ticketing/ticketManager.dart';
 import 'package:ticketing/widgets/TopBar.dart';
 import 'package:ticketing/widgets/bottomNavBar.dart';
@@ -25,8 +26,24 @@ class _TicketHistory extends State<TicketHistory> {
             if (snapshot.hasData) {
               if (snapshot.data.length > 0)
                 return ListView(
-                    children:
-                        snapshot.data.map((ticket) => Text(ticket)).toList());
+                    children: snapshot.data
+                        .map((ticket) => Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Container(
+                                padding: EdgeInsets.all(8),
+                                child: Text(
+                                  ticket,
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                decoration: BoxDecoration(
+                                  color: kSpinalcomAccent,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(4),
+                                  ),
+                                ),
+                              ),
+                            ))
+                        .toList());
               return Text('Aucun ticket declaré');
             }
 
